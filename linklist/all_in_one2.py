747fe836e79772c84029a1f8f5378cb188ee8394
genesisblock3301 <nur15-1463@diu.edu.bd>
2022-05-31 10:55:41 +0600

    linklist

--- linklist/all_in_one2.py
+++ linklist/all_in_one2.py
@@ -1,5 +1,3 @@
-from os import nice
-
 
 class Node:
     def __init__(self, data=None, next=None) -> None:
@@ -41,6 +39,10 @@ class LinkList:
             self.append_to_tail(element)
         return self.head
 
+    """ 
+    1. Remove duplicate from from unsorted link list.
+    """
+
     def remove_duplicate(self):
         s = set()
         if self.head is None:
@@ -55,27 +57,47 @@ class LinkList:
                     s.add(current_node.data)
                     previous_node = current_node
                 current_node = current_node.next
+    """ 
+    2. Revers exiting linklist
+    """
 
     def reverse(self):
         previous_node = None
         current_node = self.head
+        next_node = None
         while current_node != None:
+            # print("==",current_node.data)
+            next_node = current_node.next
             current_node.next = previous_node
             previous_node = current_node
-            # previous_node.next = current_node
-            current_node = current_node.next
+            current_node = next_node
         self.head = previous_node
-    
-    def print_linklist(self):
-        current_node = self.head
-        while current_node:
-            print(current_node.data, end=" ")
-            current_node = current_node.next
 
+    """ 
+    3. Return Kth to last. Implement an algorithm to find the Kth to last
+    element of link list
+    """
+
+    def kth_to_last_iter(self, k):
+        # print(head.data)
+        current_node = self.head
+        c = 1
+        while current_node:
+            if c == k:
+                self.head = current_node
+                self.head.next = current_node.next
+                break
+            c+=1
+            current_node = current_node.next
+        return self.head
+    
+    # def kth_to_last_rec(self,k):
+        
 
 link_list = LinkList()
 link_list.insert_list([1, 2, 3, 4])
 # link_list.remove_duplicate()
-link_list.reverse()
+# link_list.reverse()
 print(link_list)
-print(link_list.print_linklist())
+print(link_list.kth_to_last(2))
+print(link_list)
