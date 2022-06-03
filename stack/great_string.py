bf0ab9fd497f2ae3e987054fca29e289415a4b47
genesisblock3301 <nur15-1463@diu.edu.bd>
2022-06-03 10:47:53 +0600

    stack

---
+++ stack/great_string.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/make-the-string-great/
+
+
+def great_string(s):
+    stack = []
+    for i in s:
+        if stack:
+            if ((ord(stack[-1]) != ord(i)) and (stack[-1].lower() == i.lower())):
+                stack.pop()
+            else:
+                stack.append(i)
+        else:
+            stack.append(i)
+            
+    return "".join(stack)
+
+
+# s = "leEeetcode"
+# s = "abBAcC"
+# s = "Pp"
+s = 'Hvh'
+print(great_string(s))
