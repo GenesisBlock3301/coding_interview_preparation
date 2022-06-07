9a56fc8544ed5d40fd78c43aa3e1ff847b019775
genesisblock3301 <nur15-1463@diu.edu.bd>
2022-06-07 18:49:20 +0600

    dynamic algo

---
+++ array_string/longest_palindrome.py
@@ -0,0 +1,22 @@
+
+""" 
+https://leetcode.com/problems/longest-palindrome/
+In every palindrome has only one odd number, rest of are even number.
+"""
+def longest_palindrome(s):
+    alpha = [0]*128
+    n = len(s)
+    for i in s:
+        alpha[ord(i)] += 1
+    odd_count = 0
+    for i in alpha:
+        if i%2 != 0:
+            odd_count+=1
+        
+    return n-(odd_count-1) if odd_count > 1 else n
+
+
+# s = "abccccdd"
+# s ="aaaAaaaa"
+s = "civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth"
+print(longest_palindrome(s))
