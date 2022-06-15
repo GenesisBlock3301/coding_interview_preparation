8f21725c2c938c38fb1df61fc5de231e07b08a29
genesisblock3301 <nur15-1463@diu.edu.bd>
2022-06-15 11:09:55 +0600

    array

---
+++ array_string/Transform_rank.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/rank-transform-of-an-array/
+from collections import OrderedDict
+def transform_rank(nums):
+    dic = dict()
+    c = 0
+    n = len(nums)
+    for i in sorted(nums):
+        if i not in dic:
+            c+=1
+            dic[i] = c
+
+    # val = []
+
+    for i in range(len(nums)):
+        if nums[i] in dic:
+            nums[i] = dic[nums[i]]
+            # val.append(dic[i])
+    return nums
+
+
+# nums = [40,10,20,30]
+nums = [100,100,100]
+print(transform_rank(nums))
