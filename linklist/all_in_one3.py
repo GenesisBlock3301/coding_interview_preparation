54b449f1845b0eef052074732ca18ab6541cb264
sifat3301 <nur.amin@brainstation-23.com>
2022-06-26 13:05:38 +0600

    linklist

--- linklist/all_in_one3.py
+++ linklist/all_in_one3.py
@@ -1,8 +1,7 @@
-
-from asyncio import current_task
-
-
+# https://leetcode.com/problems/sort-list/
 class ListNode:
+
+
     def __init__(self, val=0, next=None) -> None:
         self.val = val
         self.next = next
