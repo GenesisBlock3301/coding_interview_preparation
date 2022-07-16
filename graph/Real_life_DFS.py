9fbc78a99ebc41961bdc62f52595b18b5bdbc0fb
sifat3301 <nur.amin@brainstation-23.com>
2022-07-16 12:45:14 +0600

    Graph: graph

---
+++ graph/Real_life_DFS.py
@@ -0,0 +1,29 @@
+def is_distination(destination):
+    return destination == "Dhaka"
+
+
+def DFS(graph, starting_point, visited=None):
+    if visited is None:
+        visited = list()
+    if "Dhaka" in visited:
+        return
+    visited.append(starting_point)
+    # print(starting_point)
+    for next in graph[starting_point]:
+        if next not in visited:
+            if is_distination(next):
+                visited.append(next)
+                return True
+            else:
+                DFS(graph, next, visited)
+    return visited
+
+# second shortest path problem.
+
+graph = dict()
+graph["Jamalpur"] = ["Mymensingh", "Tangail"]
+graph["Mymensingh"] = ["Gazipur"]
+graph["Dhaka"] = []
+graph["Tangail"] = ["Dhaka"]
+graph["Gazipur"] = ["Dhaka"]
+print(DFS(graph,"Jamalpur"))
--- graph/real_life_BFS.py
+++ graph/real_life_BFS.py
@@ -1,22 +1,23 @@
+
 def is_distination(destination):
     return destination == "Dhaka"
 
 
-def DFS(graph, starting_point, visited=None):
-    if visited is None:
-        visited = list()
-    if "Dhaka" in visited:
-        return
-    visited.append(starting_point)
-    # print(starting_point)
-    for next in graph[starting_point]:
-        if next not in visited:
-            if is_distination(next):
-                visited.append(next)
+def search_short_path(graph, name):
+    visited = list()
+    search_queue = list()
+    search_queue += graph[name]
+    while search_queue:
+        district = search_queue.pop(0)
+        if district not in visited:
+            # print(person, end="->")
+            if is_distination(district):
+                print(f"Person name is {district}", visited)
                 return True
             else:
-                DFS(graph, next, visited)
-    return visited
+                search_queue += graph[district]
+                visited.append(district)
+    return False
 
 graph = dict()
 graph["Jamalpur"] = ["Mymensingh", "Tangail"]
@@ -24,4 +25,5 @@ graph["Mymensingh"] = ["Gazipur"]
 graph["Dhaka"] = []
 graph["Tangail"] = ["Dhaka"]
 graph["Gazipur"] = ["Dhaka"]
-print(DFS(graph,"Jamalpur"))
+
+search_short_path(graph, "Jamalpur")
