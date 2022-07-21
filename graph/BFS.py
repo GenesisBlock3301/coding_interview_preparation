6f5bbeafa544165a89662c262cfb49a257bd1af0
sifat3301 <nur.amin@brainstation-23.com>
2022-07-21 15:19:55 +0600

    Graph: graph

--- graph/BFS.py
+++ graph/BFS.py
@@ -1,33 +1,18 @@
-from collections import deque
+
+def bfs(graph, source):
+    visited = set()
+    queue = list()
+    queue.append(source)
+    visited.add(source)
+    while queue:
+        vertex = queue.pop(0)
+        print(f"{vertex}", end=" ")
+        for n in graph[vertex]:
+            if n not in visited:
+                visited.add(n)
+                queue.append(n)
 
 
-def search(graph,name):
-    visited = list()
-    search_queue = list()
-    search_queue += graph[name]
-    while search_queue:
-        person = search_queue.pop(0)
-        if person not in visited:
-            # print(person, end="->")
-            if is_mongo_seller(person):
-                print(f"Person name is {person}", visited)
-                return True
-            else:
-                search_queue += graph[person]
-                visited.append(person)
-    return False
-
-
-def is_mongo_seller(name):
-    return name[-1] == "m"
-graph = dict()
-graph["you"] = ["alice", "bob", "claire"]
-graph["bob"] = ["anuj", "peggy"]
-graph["alice"] = ["peggy"]
-graph["claire"] = ["thom", "jonny"]
-graph["anuj"] = []
-graph["peggy"] = []
-graph["thom"] = []
-graph["jonny"] = []
-
-search(graph, "you")
+graph = {0: [1, 2], 1: [2], 2: [3], 3: [1, 2]}
+print("Following is Breadth First Traversal: ")
+bfs(graph, 0)
