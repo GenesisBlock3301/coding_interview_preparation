d656fdffe73ab2b980e2231eafbc13f560949570
genesisblock3301 <nur15-1463@diu.edu.bd>
2023-06-12 10:37:27 +0600

    sorting

--- sorting_&_searching/go_/counting_sort.go
+++ sorting_&_searching/go_/counting_sort.go
@@ -1,3 +1,36 @@
 package main
 
+import (
+	"fmt"
+	"sort"
+)
+
 // counting sort time complexity O(m+n)
+
+func CountingSort(arr []int) []int {
+	length := len(arr)
+	Output := make([]int, length)
+	sort.Ints(arr)
+	MaxVal := arr[length-1]
+	Count := make([]int, MaxVal+1)
+	for _, val := range arr {
+		Count[val]++
+	}
+	for i := 1; i < length; i++ {
+		Count[i] += Count[i-1]
+	}
+	i := length - 1
+	for i >= 0 {
+		val := Count[arr[i]]
+		fmt.Println(val, arr[i])
+		Output[val-1] = arr[i]
+		Count[arr[i]]--
+		i--
+	}
+	return Output
+}
+
+func main() {
+	arr := []int{4, 2, 2, 8, 3, 3, 1}
+	fmt.Println(CountingSort(arr))
+}
--- sorting_&_searching/go_/quick_sort.go
+++ sorting_&_searching/go_/quick_sort.go
@@ -1,21 +1,24 @@
 package main
 
-func LeftRightValue(arr []int) []int {
-
-}
+import "fmt"
 
 func QuickSort(arr []int) []int {
-	if len(arr) == 1 {
+	if len(arr) <= 1 {
 		return arr
 	}
 	pivot := arr[0]
 	var LeftArray, RightArray []int
-	for _, val := range arr {
+	for _, val := range arr[1:] {
 		if val <= pivot {
 			LeftArray = append(LeftArray, val)
 		} else {
 			RightArray = append(RightArray, val)
 		}
 	}
-	return QuickSort(LeftArray)
+	return append(append(QuickSort(LeftArray), pivot), QuickSort(RightArray)...)
+}
+
+func main() {
+	b := []int{9, 7, 10, 8, 9}
+	fmt.Println(QuickSort(b))
 }
