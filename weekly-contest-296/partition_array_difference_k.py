1619192251f541209e7c6f0cc0cc7472d0dc76c6
genesisblock3301 <nur15-1463@diu.edu.bd>
2023-12-27 22:36:13 +0600

    weekly contest

--- weekly-contest-296/partition_array_difference_k.py
+++ weekly-contest-296/partition_array_difference_k.py
@@ -1,6 +1,6 @@
 # https://leetcode.com/problems/partition-array-such-that-maximum-difference-is-k/
 
-def partition_find_difference(nums,k):
+def partition_find_difference(nums, k):
     nums.sort()
     n = len(nums)
     ans = 1
@@ -10,9 +10,10 @@ def partition_find_difference(nums,k):
         if nums[i] - start > k:
             ans += 1
             start = nums[i]
-        i+=1
+        i += 1
     return ans
 
-nums = [3,6,1,2,5]
+
+nums = [3, 6, 1, 2, 5]
 k = 2
-print(partition_find_difference(nums,k))
+print(partition_find_difference(nums, k))
