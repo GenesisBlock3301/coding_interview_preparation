ba2d81be19edcf877a62623559a1cbac510cdabc
genesisblock3301 <nur15-1463@diu.edu.bd>
2024-01-15 12:11:35 +0600

    database

--- database/b-tree.py
+++ database/b-tree.py
@@ -1,9 +1,9 @@
-
 """
 A B-tree is a data structure used for efficiently storing and
 retrieving large amounts of data. It is commonly used in database
 systems and file systems, where it allows for fast access to data and efficient disk usage.
 """
+
 
 class BTreeNode:
     """ B tree format"""
@@ -23,10 +23,10 @@ class BTree:
 
     def insert_node(self, key):
         root = self.root
-        if len(root.keys) == (2*self.degree) - 1:
+        if len(root.keys) == (2 * self.degree) - 1:
             temp = BTreeNode()
             self.root = temp
-            temp.children.insert(0,root)
+            temp.children.insert(0, root)
             self.split_child(temp, 0)
             self.insert_non_full(temp, key)
         else:
@@ -37,26 +37,26 @@ class BTree:
         child = node.children[i]
         b_node = BTreeNode(node.leaf)
         node.children.insert(i, b_node)
-        node.keys.insert(i,child.keys[degree-1])
-        b_node.keys = child.keys[degree:(2*degree)-1]
+        node.keys.insert(i, child.keys[degree - 1])
+        b_node.keys = child.keys[degree:(2 * degree) - 1]
         child.keys = child.keys[0:degree - 1]
         if not child.leaf:
-            b_node.children = child.children[degree:2*degree]
+            b_node.children = child.children[degree:2 * degree]
             child.children = child.children[0, degree - 1]
 
-    def insert_non_full(self,node, key):
+    def insert_non_full(self, node, key):
         i = len(node.keys) - 1
         if node.leaf:
             node.keys.append(None, None)
             while i >= 0 and key[0] < node.keys[i][0]:
                 i -= 1
-            node.keys[i+1] = key
+            node.keys[i + 1] = key
         else:
             while i >= 0 and key[0] < node.keys[i][0]:
                 i -= 1
             i += 1
-            if len(node.children[i].keys) == (2*self.degree) - 1:
-                self.split_child(node,i)
+            if len(node.children[i].keys) == (2 * self.degree) - 1:
+                self.split_child(node, i)
                 if key[0] > node.keys[i][0]:
                     i += 1
             self.insert_non_full(node.children[i], key)
@@ -65,17 +65,13 @@ class BTree:
         if node is not None:
             i = 0
             while i < len(node.keys) and key > node.keys[i][0]:
-                i+=1
+                i += 1
 
             if i < len(node.keys) and key == node.keys[i][0]:
-                return node,i
+                return node, i
             elif node.leaf:
                 return None
             else:
                 return self.search_key(key, node.children[i])
         else:
             return self.search_key(key, self.root)
-
-
-
-
